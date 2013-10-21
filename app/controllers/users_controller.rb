f600344db410ce0c445d607b466e2d2455572123
Harrison Powers <fijimunkii@gmail.com>
2013-10-21 17:06:07 -0400

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    if User.where(email: params[:user][:email]).first
+      flash[:error] = "That email is already registered."
+      render :new
+    end
+    @user = User.new params[:user]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+      flash[:notice] = "You have successfully created an account and have been logged in, #{@user.email}! "
+    else
+      flash[:error] = "Your passwords do not match."
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
 
 end
