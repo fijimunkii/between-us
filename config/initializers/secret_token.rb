24b1dc4b33ab17291553498723c4a5bf657f6fcf
Harrison Powers <fijimunkii@gmail.com>
2013-10-21 11:41:39 -0400

    new rails app between us

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetweenUsApp::Application.config.secret_token = 'f11fa701101f59066eeaf3df1e48800a3adf7236644fac868fe20edd55550282936dde30fd7012a963b566d7abf87fecdda7ce005f27867664f1381ee6e34aa2'
