b5d95b75279ef3f0d2a22b83bac26686bf14ecdd
Harrison Powers <fijimunkii@gmail.com>
2013-10-21 18:55:06 -0400

    always b cryptin

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,10 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :location, :latitude, :longitude, :password, :password_confirmation
+  has_secure_password
   has_many :searches, :dependent => :destroy
-  geocoded_by :location, :ip_address
+  geocoded_by :ip_address
+  after_create :geocode
+  geocoded_by :location
   after_validation :geocode, :reverse_geocode, :if => :location_changed?
 end
