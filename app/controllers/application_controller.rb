22ed9db126166acf399fea04d726c2fbe5fdaa5d
Harrison Powers <fijimunkii@gmail.com>
2013-10-21 17:04:12 -0400

    current user helper method on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+
+  def current_user
+    #find user if there is a session_id
+    User.find session[:user_id] if session[:user_id]
+  end
+
 end
