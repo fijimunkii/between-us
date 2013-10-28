5571ab961d9d577c8f2d292b696dcd6aca667890
Harrison Powers <fijimunkii@gmail.com>
2013-10-28 16:00:22 -0400

    asset precompile to false

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module BetweenUsApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # ran into some heroku error--- trying this out
+    config.assets.initialize_on_precompile = false
   end
 end
